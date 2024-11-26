0cdd53ebf7af7013e779f577c55ecdcd57ae3b8a
theKrZysi3k <85125532+theKrZysi3k@users.noreply.github.com>
2024-11-26 20:25:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector(".go").addEventListener('click', () => {
+document.querySelector(".go").addEventListener('click', async () => {
     var firstname = document.querySelector(".firstname");
     var surname = document.querySelector(".surname");
     var imageInput = document.querySelector(".image");
@@ -10,18 +10,45 @@ document.querySelector(".go").addEventListener('click', () => {
     }
 
     var file = imageInput.files[0];
-    var reader = new FileReader();
 
-    reader.onload = function(event) {
-        var imageBase64 = event.target.result;
-        var params = new URLSearchParams();
-        params.set("firstname", firstname.value);
-        params.set("surname", surname.value);
-        params.set("image", imageBase64); 
-        params.set("date", date.value);
+    try {
+        // Wczytujemy plik jako base64
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
 
-        location.href = "id.html?" + params.toString();
-    };
+        reader.onload = async function () {
+            var base64Image = reader.result.split(",")[1]; // Usuwamy prefix
 
-    reader.readAsDataURL(file);
+            // Wysyłamy obraz do Imgura
+            var response = await fetch("https://api.imgur.com/3/image", {
+                method: "POST",
+                headers: {
+                    Authorization: "bc9ef004b1b3d27", // Zamień "YOUR_CLIENT_ID" na swój Client ID
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    image: base64Image,
+                    type: "base64"
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error("Wystąpił błąd podczas przesyłania obrazu na Imgur.");
+            }
+
+            var result = await response.json();
+            var imageUrl = result.data.link; // URL obrazu na Imgura
+
+            // Przekierowanie z parametrami
+            var params = new URLSearchParams();
+            params.set("firstname", firstname.value);
+            params.set("surname", surname.value);
+            params.set("image", imageUrl); // Link do obrazu
+            params.set("date", date.value);
+
+            location.href = "id.html?" + params.toString();
+        };
+    } catch (error) {
+        alert("Wystąpił błąd: " + error.message);
+    }
 });
